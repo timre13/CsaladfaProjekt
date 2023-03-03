ece3d29d0a19efdee91056f69248b9b886f2c62a
timre13 <itorteli13@gmail.com>
2023-03-03 10:44:54 +0100

    Add project files.

---
+++ Database.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.Windows.Documents;
+
+namespace DB
+{
+    public class DB
+    {
+        private SQLiteConnection _conn;
+
+        public DB()
+        {
+            SQLiteConnectionStringBuilder builder = new SQLiteConnectionStringBuilder();
+            builder.DataSource = "database.db";
+            _conn = new SQLiteConnection(builder.ConnectionString);
+            _conn.Open();
+            Debug.WriteLine("DB connection opened");
+        }
+
+        /*
+        public string GetPersonName(int id)
+        {
+            var reader = ExecReaderCmd($"SELECT name, age FROM people WHERE id = {id}");
+            var list = new List<string>();
+            while (reader.Read())
+            {
+                list.Add(reader.GetString(0));
+            }
+        }
+        */
+
+        /*
+        public void AddPerson(string name)
+        {
+            ExecWriterCmd($"INSERT INTO people VALUES (name={name}");
+        }
+        */
+
+        private SQLiteDataReader ExecReaderCmd(string command)
+        {
+            Debug.WriteLine($"Executing reader command: \"{ command }\"");
+            using (SQLiteCommand cmd = _conn.CreateCommand())
+            {
+                cmd.CommandText = command;
+                return cmd.ExecuteReader();
+            }
+        }
+
+        private int ExecWriterCmd(string command)
+        {
+            Debug.WriteLine($"Executing writer command: \"{command}\"");
+            using (SQLiteCommand cmd = _conn.CreateCommand())
+            {
+                cmd.CommandText = command;
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        ~DB()
+        {
+            _conn.Close();
+            Debug.WriteLine("DB connection closed");
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Csaladfa
+{
+    class Person
+    {
+        public string FirstName { get; private set; } = "";
+        public string LastName { get; private set; } = "";
+
+        public void Draw()
+        {
+            var rect = new Polygon();
+            rect.Fill = Brushes.Red;
+            rect.Stroke = new LinearGradientBrush(Colors.Green, Colors.Blue, 10);
+            rect.StrokeThickness = 5;
+            rect.Points.Add(new Point(100, 100));
+            rect.Points.Add(new Point(300, 100));
+            rect.Points.Add(new Point(300, 300));
+            rect.Points.Add(new Point(100, 300));
+            //canvas.Children.Add(rect);
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Person person = new Person();
+            person.Draw();
+        }
+    }
+}
