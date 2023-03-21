a3d0c1a78a4863ba36bc3759cc168ae6f5bc22ef
timre13 <itorteli13@gmail.com>
2023-03-21 07:30:48 +0100

    Disable canvas

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -35,7 +35,7 @@ namespace Csaladfa
 
         public void Draw(Canvas canvas, Person pers, int x, int y)
         {
-
+            /*
             var rect = new Polygon();
             rect.Fill = pers.GenderToBrush();
             rect.Stroke = Brushes.Gray;
@@ -63,6 +63,7 @@ namespace Csaladfa
             Canvas.SetLeft(dateText, x - CanvasPanX + 10);
             Canvas.SetTop(dateText, y - CanvasPanY + 30);
             canvas.Children.Add(dateText);
+            */
         }
 
         public MainWindow()
