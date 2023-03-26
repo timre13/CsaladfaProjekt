8fd34b7e921688ba2bf9b0a9a581c0ac60e19d08
timre13 <itorteli13@gmail.com>
2023-03-26 17:02:28 +0200

    Relationship editor: load all info

--- RelationshipEditWindow.xaml.cs
+++ RelationshipEditWindow.xaml.cs
@@ -2,8 +2,10 @@
 
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -55,6 +57,31 @@ namespace Csaladfa
             MarriagePlaceCombobox.SelectedIndex = settlements
                 .Select((v, i) => new { index = i, value = v })
                 .Where(x => x.value.id == rel.location).FirstOrDefault()?.index ?? 0;
+
+            MarriageDateYearInput.Text = rel.start_year?.ToString() ?? "";
+            DivorceDateYearInput.Text = rel.end_year?.ToString() ?? "";
+
+            var months = new List<string> { "???" };
+            months.AddRange(Enumerable.Range(1, 12).Select(x => new DateTime(2000, x, 1).ToString("MMMM", CultureInfo.CreateSpecificCulture("hu"))));
+            MarriageDateMonthInput.ItemsSource = months;
+            DivorceDateMonthInput.ItemsSource = months;
+            MarriageDateMonthInput.SelectedIndex = (int)(rel.start_month ?? 0);
+            DivorceDateMonthInput.SelectedIndex = (int)(rel.end_month ?? 0);
+
+            var days = new List<string> { "???" };
+            days.AddRange(Enumerable.Range(1, 31).Select(x => x.ToString()));
+            MarriageDateDayInput.ItemsSource = days;
+            DivorceDateDayInput.ItemsSource = days;
+            MarriageDateDayInput.SelectedIndex = (int)(rel.start_day ?? 0);
+            DivorceDateDayInput.SelectedIndex = (int)(rel.end_day ?? 0);
+
+            IsLegalCheckBox.IsChecked = rel.legal;
+        }
+
+        private static readonly Regex _yearRegex = new Regex("[0-9]+");
+        private void StartOrEndDateYearInput_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !_yearRegex.IsMatch(e.Text) || ((sender as TextBox)!.Text.Length == 0 && e.Text[0] == '0');
         }
     }
 }
