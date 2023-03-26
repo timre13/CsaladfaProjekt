b4e65eef5184fc8ce782379a6462f4ed81aa1fe3
timre13 <itorteli13@gmail.com>
2023-03-26 18:52:21 +0200

    Display '-' when date is null or zero

--- Database.cs
+++ Database.cs
@@ -572,6 +572,9 @@ namespace DB
 
         public static string DateToString(long? year, long? month, long? day)
         {
+            if ((year ?? 0) == 0 && (month ?? 0) == 0 && (day ?? 0) == 0)
+                return "-";
+
             string yearStr = (year == null ? "????" : year!.ToString()!.PadLeft(4, '0'));
             string monthStr = (month == null ? "??" : month!.ToString()!.PadLeft(2, '0'));
             string dayStr = (day == null ? "??" : day!.ToString()!.PadLeft(2, '0'));
